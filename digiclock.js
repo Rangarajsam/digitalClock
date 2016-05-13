d219ab2a9b92913b7cc24fb2d4ef2d36e8070fe1
Rangaraj <x0220422@ti.com>
2016-05-13 15:06:19 +0530

    digital clock completed with id selector

--- digiclock.js
+++ digiclock.js
@@ -1,17 +1,39 @@
 var digiClock=(function(){
   
   return{
-    
-    time:function(){
+    time:function(element){
       var currentTime= new Date();
       var hours= currentTime.getHours();
       var minutes = currentTime.getMinutes();
       var seconds=currentTime.getSeconds();
-      var clockDiv= document.getElementById('clock');
-      clockDiv.innerText= hours+' : '+minutes+' : '+seconds;
+      var meridiem = 'AM';
+      if(seconds<10){
+        seconds='0'+seconds;
+      }
+      if(minutes<10){
+        minutes='0'+minutes;
+      }
+      if(hours<10){
+        hours='0'+hours;
+      }
+      if(hours===12){
+        meridiem='PM';
+      }
+      else if(hours>12){
+        hours=hours-12;
+        meridiem='PM';
+      }
+      if(hours===0){
+        hours=12;
+      }
+      var clockDiv= document.getElementById(element);
+      clockDiv.innerText= hours+' : '+minutes+' : '+seconds+" "+meridiem;
     },
-    tick:function(){
-      setInterval(this.time, 1000);
+    tick:function(element){
+      var userObject=this;
+      setInterval(function(){
+        userObject.time(element);
+      }, 1000);
     }
     
   };
